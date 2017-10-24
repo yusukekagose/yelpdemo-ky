0dffc1930a525ee404c0a21ba6741f9f9a1f436d
kagose yusuke <>
2017-10-24 17:59:52 +0900

    added review model added user reference

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,6 @@
 class ReviewsController < ApplicationController
   before_action :set_review, only: [:show, :edit, :update, :destroy]
-
+  before_action :authenticate_user!
   # GET /reviews
   # GET /reviews.json
   def index
@@ -69,6 +69,6 @@ class ReviewsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def review_params
-      params.require(:review).permit(:rating, :comment)
+      params.require(:review).permit(:rating, :comment).merge(user_id: current_user.id)
     end
 end
