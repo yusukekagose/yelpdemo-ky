22ee404ae1ae2a6a7911b2ec57d748f8b3710408
kagose yusuke <>
2017-10-25 10:52:42 +0900

    added data validation

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,4 +2,12 @@ class Restaurant < ApplicationRecord
   mount_uploader :image, ImageUploader
 
   has_many :reviews
+
+  validates :name, :address, :phone, :website, :image, presence: true
+  validates :phone, format: { with: /\A\(\d{3}\) \d{4}-\d{4}\z/,
+    message: "must be in the format (123) 4567-7890" }
+  validates :website, format: { with: /\Ahttps?:\/\/.*\z/,
+    message: "must start with http:// or https://" }
+  validates :address, format: { with: /\A\d+[^,]+,[^,]+, [A-Z]{2} \d{5}\z/,
+    message: "must be in the format 350 Fifth Avenue, New York, NY 10118" }
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,12 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :restaurant
+
+  validates :rating, :comment, presence: true
+  validates :rating, numericality: {
+    only_integer: true,
+    greater_than_or_equal_to: 1,
+    less_than_or_equal_to: 5,
+    message: "1から5の間で評価してください"
+  }
 end
